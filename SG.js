3db5359ef6d9d34d8037a8cf5df26cead96070f8
iigilani <iigilani@gmail.com>
2024-06-04 13:41:16 +0500

    Files added

---
+++ SG.js
@@ -0,0 +1,118 @@
+const   slides = document.querySelectorAll(".slide")
+var counter = 0;
+//console.log(slides)
+slides.forEach(
+    (slide,index) => {
+    slide.style.left = `${index * 100}%`
+    }
+)
+
+const goPrev = () => {
+    if (counter != 0) {
+        counter--;
+        slideImage();
+        }
+}
+
+const goNext = () => {
+    if (counter < slides.length - 1) {
+        counter++;
+        slideImage();
+        }
+}
+
+const slideImage = () => {
+    slides.forEach(
+        (slide) => {
+            slide.style.transform = `translateX(-${counter*100}%)`
+        }
+    )
+}
+
+const goSlide1 = () => {
+  if (counter != 0) {
+      counter = 0;
+      slideImage();
+    }
+
+  else if (counter < slides.length - 1) {
+           counter = 0;
+           slideImage();
+    }
+}
+
+const goSlide2 = () => {
+  if (counter != 0) {
+      counter = 1;
+      slideImage();
+    }
+
+  else if (counter < slides.length - 1) {
+           counter = 1;
+           slideImage();
+    }
+}
+
+const goSlide3 = () => {
+  if (counter != 0) {
+      counter = 2;
+      slideImage();
+    }
+
+  else if (counter < slides.length - 1) {
+           counter = 2;
+           slideImage();
+    }
+}
+
+const goSlide4 = () => {
+  if (counter != 0) {
+      counter = 3;
+      slideImage();
+      }
+  else if (counter < slides.length - 1) {
+           counter = 3;
+           slideImage();
+    }
+}
+
+const goSlide5 = () => {
+  if (counter < slides.length - 1) {
+      counter = 4;
+      slideImage();
+      }
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName(".dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show2");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn2')) {
+      var dropdowns = document.getElementsByClassName(".dropdown-content2");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show2')) {
+          openDropdown.classList.remove('show2');
+        }
+      }
+    }
+  }
